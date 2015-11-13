fad0051c5ba698ed7f8e91c5f7670ae4f4a7b0e6
kattamuruphani <kattamuruphani1616@gmail.com>
2015-11-13 19:52:54 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -16,10 +16,12 @@ NOTES: Don't create new array, try to change the input array.
 void remove(int*,int);
 
 void sort(int*, int, int);
-
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	sort(Arr, 0, len-1);
+	if (len <= 0 || Arr == NULL){
+		return NULL;
+	}
+	sort(Arr, 0, len - 1);
 	remove(Arr, len);
 	return Arr;
 }
@@ -58,24 +60,22 @@ void sort(int *x, int first, int last){
 
 
 void remove(int *a, int len){
-	int i,x,j=0;
+	int i, x, j = 0;
 	int *binary;
-	binary = (int*)malloc(sizeof(int)*(len + 1));
+	binary = (int*)malloc(sizeof(int)*(len));
 	for (i = 0; i < len; i++){
 		x = a[i];
 		binary[i] = 1;
-		while (x == a[i+1]){
+		while (x == a[i + 1]){
 			i++;
 			binary[i] = 0;
 		}
 	}
-	for (i = 0, j = 0; i < len+1; i++){
+	for (i = 0, j = 0; i < len; i++){
 		if (binary[i] == 1){
 			a[j] = a[i];
 			j++;
 		}
 	}
-	for (; j <= len; j++){
-		a[j] = 0;
-	}
+
 }
